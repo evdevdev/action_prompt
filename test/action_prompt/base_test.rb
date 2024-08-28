733fb1881c182dc925852f5d9e454374099d7bc3
Evan Arnold <evan.arnold@gmail.com>
2024-08-28 10:46:18 -0500

    add test for a prompt with locals

--- test/action_prompt/base_test.rb
+++ test/action_prompt/base_test.rb
@@ -5,4 +5,9 @@ class ActionPrompt::BaseTest < ActiveSupport::TestCase
     result = ActionPrompt.render("hello_world")
     assert_equal "You are a helpful assistant who replies with, \"Hello, world!\"", result
   end
+
+  test "a template with a variable" do
+    result = ActionPrompt.render("hello_somebody", locals: { name: "John" })
+    assert_equal "You are a helpful assistant who replies with, \"Hello, John!\"", result
+  end
 end
