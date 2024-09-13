d59906593b4d54b29bbd0f0f67baffa0ca9d483b
Evan Arnold <evan.arnold@gmail.com>
2024-09-13 13:00:40 -0500

    basic tailwind styling

--- app/controllers/action_prompt/previews_controller.rb
+++ app/controllers/action_prompt/previews_controller.rb
@@ -1,6 +1,11 @@
-class ActionPrompt::PreviewsController < Rails::ApplicationController
+class ActionPrompt::PreviewsController < ActionController::Base
   prepend_view_path ActionPrompt::Engine.root.join("app", "views")
-  before_action :require_local!
+  layout "application"
+  # Ideally, we'd like to inhert from Rails::ApplicationController, but that
+  # would prevent us from using a Tailwind CDN. So instead, we're using ActionController::Base.
+  #
+  # If we are able to use Rails::ApplicationController, then re-enable this line
+  # before_action :require_local!
 
   def index
     @page_title = "Action Prompt Previews"
