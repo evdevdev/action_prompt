f1943e28a464da1c59684ac65287bd63b4850ba9
Alexander Senko <Alexander.Senko@gmail.com>
2025-06-09 12:09:38 +0700

    Optimization: reuse a controller for rendering

--- lib/action_prompt/renderer.rb
+++ lib/action_prompt/renderer.rb
@@ -1,13 +1,12 @@
 module ActionPrompt
   class Renderer
     def initialize
-      # NOOP
+      @controller = ApplicationController.new
+      @controller.prepend_view_path(Rails.root.join("app", "prompts"))
     end
 
     def render(template_name, locals: {})
-      controller = ApplicationController.new
-      controller.prepend_view_path(Rails.root.join("app", "prompts"))
-      controller.render_to_string(template: template_name, locals: locals, layout: false)
+      @controller.render_to_string(template: template_name, locals: locals, layout: false)
     end
   end
 end
